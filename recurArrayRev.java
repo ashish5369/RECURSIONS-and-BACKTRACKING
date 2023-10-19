00ac054450e830b7aa1dc999d21fa13fb88f9a27
ashish5369 <ashish.21BCE7889@vitapstudent.ac.in>
2023-10-20 01:13:46 +0530

    4

--- recurArrayRev.java
+++ recurArrayRev.java
@@ -1,5 +1,32 @@
+//print array wihtour using loop
+
+import java.util.Scanner;
+
 public class recurArrayRev {
+    // expectation is ki prints from n to the last
+    // faith is that ki yeh n+1 se last tak print karna jaanta hai
+    public static void displayArray(int arr[], int idx) {
+        if (idx == -1) {
+            return;
+        }
+        System.out.println(arr[idx]);
+        displayArray(arr, idx - 1);
+
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // gettting the input intto the array
+
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        displayArray(arr, arr.length - 1);
+        sc.close();
+
     }
 }
