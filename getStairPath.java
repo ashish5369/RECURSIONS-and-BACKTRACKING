b2b1dc27e9a1429c9ee29d140abe7659106168cc
ashish5369 <ashish.21BCE7889@vitapstudent.ac.in>
2023-10-24 03:15:17 +0530

    8

--- getStairPath.java
+++ getStairPath.java
@@ -1,8 +1,45 @@
+
+//refernce video :- https://www.youtube.com/watch?v=hMJAlbJIS7E&list=PL-Jc9J83PIiFxaBahjslhBD1LiJAV7nKs&index=29&t=353s&ab_channel=Pepcoding
 // we are on the nth stair and in one go we can have either 1 2 or 3 stairs can be jumped 
 // and we need to have all the possible cominations that we can use to go down the stairs
+import java.util.*;
 
 public class getStairPath {
-    public static void main(String[] args) {
 
+    public static ArrayList<String> getStairPaths(int n) {
+        if (n == 0) {
+            ArrayList<String> bres = new ArrayList<>();
+            bres.add("");
+            return bres;
+        } else if (n < 0) {
+            ArrayList<String> bres = new ArrayList<>();
+            return bres;
+
+        }
+
+        ArrayList<String> paths1 = getStairPaths(n - 1);
+        ArrayList<String> paths2 = getStairPaths(n - 2);
+        ArrayList<String> paths3 = getStairPaths(n - 3);
+
+        ArrayList<String> paths = new ArrayList<>();
+        for (String path : paths1) {
+            paths.add(1 + path);
+        }
+        for (String path : paths2) {
+            paths.add(2 + path);
+        }
+        for (String path : paths3) {
+            paths.add(3 + path);
+        }
+        return paths;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<String> paths = getStairPaths(n);
+        System.out.println(paths);
+        sc.close();
     }
 }
