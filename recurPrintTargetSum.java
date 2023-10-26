b3ab509325c477930dd7517e5c0b582372092efa
ashish5369 <ashish.21BCE7889@vitapstudent.ac.in>
2023-10-27 04:44:42 +0530

    10

---
+++ recurPrintTargetSum.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class recurPrintTargetSum {
+    public static void printTargetSumSubsets(int[] arr, int idx, String set, int sos, int tar) {
+        if (idx == arr.length) {
+            if (sos == tar) {
+                System.out.println(set + ".");
+            }
+            return;
+        }
+        printTargetSumSubsets(arr, idx + 1, set + arr[idx] + ",", sos + arr[idx], tar);
+
+        printTargetSumSubsets(arr, idx + 1, set, sos, tar);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int tar = sc.nextInt();
+        printTargetSumSubsets(arr, 0, "", 0, tar);
+
+        sc.close();
+    }
+}
